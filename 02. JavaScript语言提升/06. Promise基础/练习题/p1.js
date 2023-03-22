1b9534df3f0cdb304f4cdabb5f1be9011817ae5c
tuotuo88888888 <2646218690@qq.com>
2023-03-22 22:58:28 +0800

    ++

--- 02. JavaScript语言提升/06. Promise基础/练习题/p1.js
+++ 02. JavaScript语言提升/06. Promise基础/练习题/p1.js
@@ -5,8 +5,12 @@
  * @param {Number} duration 等待的时间
  * @returns {Promise} 返回一个任务，该任务在指定的时间后完成
  */
-function delay(duration) {}
+function delay(duration) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+}
 
 // 2. 按照要求，调用delay函数，完成程序
-
+delay(1000).then(() => console.log("finish"));
 // 利用delay函数，等待1秒钟，输出：finish
