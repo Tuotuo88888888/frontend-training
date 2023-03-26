f7627682152827e15fc7d2b61211ddd428285684
tuotuo88888888 <2646218690@qq.com>
2023-03-26 15:40:49 +0800

    ++

--- 03. 程序思维训练/01. 歌词滚动/自己写/index.js
+++ 03. 程序思维训练/01. 歌词滚动/自己写/index.js
@@ -14,51 +14,60 @@ async function getLrc() {
     audio: document.querySelector("audio"),
     ul: document.querySelector(".box ul"),
   };
+  const size = {
+    boxClientHeight: dom.box.clientHeight,
+    liHeight: 30,
+  };
+  let secondArr = [];
 
   function main() {
     bindEvents();
   }
   function bindEvents() {
     dom.audio.addEventListener("loadeddata", audioLoad);
+    dom.audio.addEventListener("timeupdate", function (e) {
+      setStatus(this.currentTime);
+    });
   }
   async function audioLoad() {
     dom.ul.innerHTML = "";
     try {
       const data = await getLrc();
-      const lineData = data.split("\n");
-      const arr = lineData.slice(1).map((i) => {
+      const lineData = data.split("\n").filter((i) => i);
+      const arr = lineData.map((i) => {
         const line = i.split("]");
-        const lineLeft = line[0].substring(1).split(/:|\./);
-        const second = lineLeft[0] * 60 + +lineLeft[1] + lineLeft[2] / 1000;
+        const lineLeft = line[0].replace("[", "").split(":");
+        const second = lineLeft[0] * 60 + +lineLeft[1];
         const word = line[1];
         return { second, word };
       });
       dom.ul.innerHTML = arr.map((i) => `<li>${i.word}</li>`).join("\n");
-      const secondArr = arr.map((i) => i.second);
-      const boxClientHeight = dom.box.clientHeight;
-      const liHeight = dom.ul.children[0].offsetHeight;
-      dom.audio.addEventListener("timeupdate", function (e) {
-        const { currentTime } = dom.audio;
-        const curIndex = secondArr.findIndex(
-          (e, i, arr) => currentTime > e && currentTime < arr[i + 1]
-        );
-        const activeLi = dom.ul.querySelector(".active");
-        if (activeLi) {
-          activeLi.className = "";
-        }
-        const currentLi = dom.ul.children[curIndex];
-        if (currentLi) {
-          currentLi.className = "active";
-        }
-        let y = curIndex * liHeight - (boxClientHeight / 2 - liHeight / 2);
-        if (y < 0) {
-          y = 0;
-        }
-        dom.ul.style.transform = `translateY(-${y}px)`;
-      });
+      secondArr = arr.map((i) => i.second);
+      size.liHeight = dom.ul.children[0].offsetHeight;
     } catch (e) {
       dom.ul.innerHTML = `<li class="active">${e.message}</li>`;
     }
+  }
+
+  function setStatus(currentTime) {
+    const curIndex = secondArr.findIndex((e) => currentTime < e) - 1;
+    if (curIndex < 0) {
+      return;
+    }
+    const activeLi = dom.ul.querySelector(".active");
+    if (activeLi) {
+      activeLi.className = "";
+    }
+    const currentLi = dom.ul.children[curIndex];
+    if (currentLi) {
+      currentLi.className = "active";
+    }
+    let y =
+      curIndex * size.liHeight - (size.boxClientHeight / 2 - size.liHeight / 2);
+    if (y < 0) {
+      y = 0;
+    }
+    dom.ul.style.transform = `translateY(-${y}px)`;
   }
   main();
 })();
