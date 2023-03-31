bf14142d3548d9a750e085c2b8c3fb99033c6922
tommytuo@apjcorp.com <12345678>
2023-03-31 13:30:14 +0800

    ++

---
+++ 04. 网络/13.聊天机器人-自己写/静态页面/js/api.js
@@ -0,0 +1,12 @@
+const BASE = "https://study.duyiedu.com";
+function reg(userInfo) {}
+
+function login(loginInfo) {}
+
+function exists(loginId) {}
+
+function profile() {}
+
+function sendChat(content) {}
+
+function getHistory() {}
