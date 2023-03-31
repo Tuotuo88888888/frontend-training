cf4f258d02fc0c0d3c71799f302398255f359c03
tommytuo@apjcorp.com <12345678>
2023-03-31 14:37:55 +0800

    ++

--- 04. 网络/13.聊天机器人-自己写/静态页面/js/api.js
+++ 04. 网络/13.聊天机器人-自己写/静态页面/js/api.js
@@ -1,5 +1,31 @@
 const BASE = "https://study.duyiedu.com";
-function reg(userInfo) {}
+const api = {
+  get(url, config = {}) {
+    const defaultConfig = {
+      method: "GET",
+    };
+    return fetch(BASE + url, { ...defaultConfig, ...config });
+  },
+  post(url, body, config = {}) {
+    const defaultConfig = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+    return fetch(BASE + url, { ...defaultConfig, ...config });
+  },
+};
+async function reg(userInfo) {
+  try {
+    const res = await api.post("/api/user/reg", userInfo);
+    const data = await res.json();
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 function login(loginInfo) {}
 
