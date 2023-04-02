da2974ef632af6abca516295f4300a2b3ea83540
tuotuo88888888 <2646218690@qq.com>
2023-04-02 22:17:33 +0800

    ++

--- 04. 网络/13.聊天机器人-自己写/静态页面/js/api.js
+++ 04. 网络/13.聊天机器人-自己写/静态页面/js/api.js
@@ -1,20 +1,31 @@
 const BASE = "https://study.duyiedu.com";
+function setToken(token) {
+  console.log(token);
+  localStorage.setItem("jwt", token);
+}
+function getToken() {
+  return localStorage.getItem("jwt");
+}
 const api = {
   get(url, config = {}) {
     const defaultConfig = {
       method: "GET",
+      headers: {
+        authorization: "Bearer " + getToken(),
+      },
     };
-    return fetch(BASE + url, { ...defaultConfig, ...config });
+    return fetch(new URL(url, BASE), { ...defaultConfig, ...config });
   },
   post(url, body, config = {}) {
     const defaultConfig = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        authorization: "Bearer " + getToken(),
       },
       body: JSON.stringify(body),
     };
-    return fetch(BASE + url, { ...defaultConfig, ...config });
+    return fetch(new URL(url, BASE), { ...defaultConfig, ...config });
   },
 };
 async function reg(userInfo) {
@@ -27,12 +38,54 @@ async function reg(userInfo) {
   }
 }
 
-function login(loginInfo) {}
+async function login(loginInfo) {
+  try {
+    const res = await api.post("/api/user/login", loginInfo);
+    const data = await res.json();
+    if (data.code === 0) {
+      setToken(res.headers.get("authorization"));
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
 
-function exists(loginId) {}
+async function exists(loginId) {
+  try {
+    const res = await api.get("/api/user/exists", loginId);
+    const data = await res.json();
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
+}
 
-function profile() {}
+async function profile() {
+  try {
+    const res = await api.get("/api/user/profile");
+    const data = await res.json();
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
+}
 
-function sendChat(content) {}
+async function sendChat(content) {
+  try {
+    const res = await api.post("/api/chat", { content });
+    const data = await res.json();
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
+}
 
-function getHistory() {}
+async function getHistory() {
+  try {
+    const res = await api.get("/api/chat/history");
+    const data = await res.json();
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
+}
