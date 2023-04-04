693f45e72f1d9c753a5c8caff89df4b97946ee3f
tommytuo@apjcorp.com <12345678>
2023-04-04 17:59:28 +0800

    ++

--- 04. 网络/13.聊天机器人-自己写/静态页面/js/api.js
+++ 04. 网络/13.聊天机器人-自己写/静态页面/js/api.js
@@ -1,69 +1,82 @@
-const BASE = "https://study.duyiedu.com";
-function setToken(token) {
-  console.log(token);
-  localStorage.setItem("jwt", token);
-}
-function getToken() {
-  return localStorage.getItem("jwt");
-}
-const api = {
-  get(url, config = {}) {
-    const defaultConfig = {
-      method: "GET",
-      headers: {
-        authorization: "Bearer " + getToken(),
-      },
-    };
-    return fetch(new URL(url, BASE), { ...defaultConfig, ...config });
-  },
-  post(url, body, config = {}) {
-    const defaultConfig = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        authorization: "Bearer " + getToken(),
-      },
-      body: JSON.stringify(body),
-    };
-    return fetch(new URL(url, BASE), { ...defaultConfig, ...config });
-  },
-};
-async function reg(userInfo) {
-  const res = await api.post("/api/user/reg", userInfo);
-  const data = await res.json();
-  return data;
-}
+var API = (function () {
+  const BASE_URL = "https://study.duyiedu.com";
+  const token = {
+    Key: "JWT",
+    setToken(token) {
+      localStorage.setItem(this.Key, token);
+    },
+    getToken() {
+      return localStorage.getItem(this.Key);
+    },
+    clearToken() {
+      localStorage.removeItem(this.Key);
+    },
+  };
+  const api = {
+    async get(url, config = {}) {
+      const defaultConfig = {
+        method: "GET",
+        headers: {
+          authorization: "Bearer " + token.getToken(),
+        },
+      };
+      return fetch(new URL(url, BASE_URL), { ...defaultConfig, ...config });
+    },
+    async post(url, body, config = {}) {
+      const defaultConfig = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: "Bearer " + token.getToken(),
+        },
+        body: JSON.stringify(body),
+      };
+      return fetch(new URL(url, BASE_URL), { ...defaultConfig, ...config });
+    },
+  };
 
-async function login(loginInfo) {
-  const res = await api.post("/api/user/login", loginInfo);
-  const data = await res.json();
-  if (data.code === 0) {
-    setToken(res.headers.get("authorization"));
-    return true;
+  async function reg(userInfo) {
+    return await api.post("/api/user/reg", userInfo).then((res) => res.json());
   }
-  return false;
-}
 
-async function exists(loginId) {
-  const res = await api.get("/api/user/exists", loginId);
-  const data = await res.json();
-  return data;
-}
+  async function login(loginInfo) {
+    const res = await api.post("/api/user/login", loginInfo);
+    const results = await res.json();
+    if (results.code === 0) {
+      token.setToken(res.headers.get("authorization"));
+    }
+    return results;
+  }
 
-async function profile() {
-  const res = await api.get("/api/user/profile");
-  const data = await res.json();
-  return data;
-}
+  async function exists(loginId) {
+    return await api
+      .get("/api/user/exists?loginId=" + loginId)
+      .then((res) => res.json());
+  }
 
-async function sendChat(content) {
-  const res = await api.post("/api/chat", { content });
-  const data = await res.json();
-  return data;
-}
+  async function profile() {
+    return await api.get("/api/user/profile").then((res) => res.json());
+  }
 
-async function getHistory() {
-  const res = await api.get("/api/chat/history");
-  const data = await res.json();
-  return data;
-}
+  async function sendChat(content) {
+    return await api.post("/api/chat", { content }).then((res) => res.json());
+  }
+
+  async function getHistory() {
+    return await api.get("/api/chat/history").then((res) => res.json());
+  }
+
+  function loginOut() {
+    token.clearToken();
+  }
+  
+  return {
+    reg,
+    login,
+    exists,
+    profile,
+    sendChat,
+    getHistory,
+    loginOut,
+  };
+})();
