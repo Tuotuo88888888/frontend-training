da050c9fd4c3cf330e018bdf33703ad381652cb0
tuotuo88888888 <2646218690@qq.com>
2023-05-08 00:50:37 +0800

    ++

---
+++ 08. vue从入门到实战/自己写/tommy-site/src/components/Empty/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="empty-container">
+    <Icon type="empty" />
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+import Icon from "../Icon";
+export default {
+  components: {
+    Icon,
+  },
+  props: {
+    text: {
+      type: String,
+      default: "无数据",
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import url("~@/styles/var.less");
+.empty-container {
+  width: 150px;
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  p {
+    color: @lightWords;
+  }
+}
+.icon-container {
+  font-size: 120px;
+  color: @lightWords;
+}
+</style>
---
+++ 08. vue从入门到实战/自己写/tommy-site/src/components/ImageLoader/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <img :src="src" alt="" class="image-loader-container" />
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      default: 500,
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.image-loader-container {
+  width: 400px;
+  height: 280px;
+  object-fit: cover;
+}
+</style>
