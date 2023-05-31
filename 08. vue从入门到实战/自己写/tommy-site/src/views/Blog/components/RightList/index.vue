4e4c0a531387689948a0a7a40971db1c8f56a308
tommytuo@apjcorp.com <12345678>
2023-05-31 18:05:31 +0800

    ++

---
+++ 08. vue从入门到实战/自己写/tommy-site/src/views/Blog/components/RightList/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul class="right-list-container">
+    <li
+      v-for="(item, i) in list"
+      :key="i"
+      :class="{ active: item.isSelect }"
+      @click="handleClick(item)"
+    >
+      <span>{{ item.name }}</span>
+    </li>
+    <RightList :list="item.children" @select="handleClick" />
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "RightList",
+  props: {
+    list: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    handleClick(item) {
+      this.$emit("select", item);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.right-list-container {
+  list-style: none;
+  padding: 0;
+  .right-list-container {
+    margin-left: 1em;
+  }
+  li {
+    height: 40px;
+    line-height: 40px;
+  }
+}
+</style>
