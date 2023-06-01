5df77af21783a9be4f5867b91e64e59a0e76f320
tommytuo@apjcorp.com <12345678>
2023-06-01 17:36:45 +0800

    ++

--- 08. vue从入门到实战/自己写/tommy-site/src/main.js
+++ 08. vue从入门到实战/自己写/tommy-site/src/main.js
@@ -9,6 +9,20 @@ Vue.prototype.$showMessage = showMessage;
 
 import vLoading from "./directives/loading";
 Vue.directive("loading", vLoading);
+
+import request from "./api/request";
+
+request.get("/api/blogtype").then((r) => console.log(r));
+request
+  .get("/api/blog", {
+    params: {
+      page: 1,
+      limit: 10,
+      categoryid: -1,
+    },
+  })
+  .then((r) => console.log(r));
+
 new Vue({
   router,
   render: (h) => h(App),
---
+++ 08. vue从入门到实战/自己写/tommy-site/src/mock/blog.js
@@ -0,0 +1,45 @@
+import Mock from "mockjs";
+import qs from "querystring";
+
+Mock.mock("/api/blogtype", "get", {
+  code: 0,
+  msg: "",
+  "data|10-20": [
+    {
+      "id|+1": 1,
+      name: "分类@id",
+      "articleCount|0-300": 0,
+      "order|+1": 1,
+    },
+  ],
+});
+
+Mock.mock(/^\/api\/blog(\?.+)?$/, "get", function (options) {
+  const {
+    page = 1,
+    limit = 10,
+    categoryid = -1,
+  } = qs.parse(options.url.split("?")?.[1]);
+  return Mock.mock({
+    code: 0,
+    msg: "",
+    data: {
+      "total|2000-3000": 0,
+      [`rows|${limit}`]: [
+        {
+          id: "@guid",
+          title: "@ctitle",
+          description: "@cparagraph(1,10)",
+          category: {
+            "id|1-10": 0,
+            name: "分类@id",
+          },
+          "scanNumber|0-3000": 0,
+          "commentNumber|0-300": 30,
+          thumb: Mock.Random.image("300x250", "#000", "#fff", "Random Image"),
+          createDate: `@date('T')`,
+        },
+      ],
+    },
+  });
+});
