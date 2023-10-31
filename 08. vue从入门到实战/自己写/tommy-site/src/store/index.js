0fcc8771137215db8ef9d6d6933c3c4db2faa532
tuotuo88888888 <2646218690@qq.com>
2023-10-31 17:45:25 +0800

    08.37

--- 08. vue从入门到实战/自己写/tommy-site/src/store/index.js
+++ 08. vue从入门到实战/自己写/tommy-site/src/store/index.js
@@ -4,12 +4,13 @@ import Vue from "vue";
 import banner from "./banner";
 import setting from "./setting";
 import about from "./about";
+import project from "./project";
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   strict: true,
-  modules: { banner, setting, about },
+  modules: { banner, setting, about, project },
 });
 
 export default store;
---
+++ 08. vue从入门到实战/自己写/tommy-site/src/views/Project/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="project-container" ref="projectContainer" v-loading="loading">
+    <a
+      v-for="item in data"
+      :key="item.id"
+      class="project-item"
+      :href="item.url ? item.url : `javascript:alert('该项目无法通过外网访问')`"
+      :target="item.url ? '_blank' : '_self'"
+    >
+      <img class="thumb" v-lazy="item.thumb" />
+      <div class="info">
+        <h2>
+          {{ item.name }}
+        </h2>
+        <a
+          class="github"
+          target="_blank"
+          :href="item.github"
+          @click.stop=""
+          v-if="item.github"
+        >
+          github
+        </a>
+        <p v-for="(desc, i) in item.description" :key="i">
+          {{ desc }}
+        </p>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import mainScroll from "@/mixins/mainScroll";
+export default {
+  mixins: [mainScroll("projectContainer")],
+  computed: mapState("project", ["loading", "data"]),
+  created() {
+    this.$store.dispatch("project/fetchProject");
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import "~@/styles/var.less";
+.project-container {
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 20px;
+  position: relative;
+  overflow-y: auto;
+  scroll-behavior: smooth;
+}
+.project-item {
+  transition: 0.5s;
+  padding: 20px;
+  display: flex;
+  margin-bottom: 20px;
+  &:hover {
+    box-shadow: -1px 1px 5px #000;
+    transform: scale(1.01) translate(3px, -3px);
+    color: inherit;
+  }
+  .thumb {
+    width: 250px;
+    min-height: 150px;
+    flex: 0 0 auto;
+    object-fit: cover;
+    border-radius: 5px;
+    margin-right: 15px;
+  }
+  .info {
+    line-height: 1.7;
+    flex: 1 1 auto;
+    h2 {
+      margin: 0;
+    }
+  }
+  .github {
+    font-size: 14px;
+    color: @primary;
+  }
+}
+</style>
