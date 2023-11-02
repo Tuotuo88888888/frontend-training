bc554ee22b2e96732cde1d95a4ef66877dc8b7ae
tuotuo88888888 <2646218690@qq.com>
2023-11-02 21:57:19 +0800

    08.38

--- 08. vue从入门到实战/自己写/tommy-site/src/views/Message/index.vue
+++ 08. vue从入门到实战/自己写/tommy-site/src/views/Message/index.vue
@@ -14,12 +14,17 @@
 import MessageArea from "@/components/MessageArea";
 import fetchData from "@/mixins/fetchData";
 import mainScroll from "@/mixins/mainScroll";
+import moreMessage from "@/mixins/moreMessage";
 import { getMessages, postMessage } from "@/api/message";
 export default {
   components: {
     MessageArea,
   },
-  mixins: [fetchData({ total: 0, rows: [] }), mainScroll("messageContainer")],
+  mixins: [
+    fetchData({ total: 0, rows: [] }),
+    mainScroll("messageContainer"),
+    moreMessage,
+  ],
   data() {
     return {
       page: 1,
@@ -35,38 +40,6 @@ export default {
       this.data.rows.unshift(resp);
       this.data.total++;
       callback("感谢您的留言");
-    },
-    async fetchMore() {
-      if (!this.hasMore) {
-        return;
-      }
-      this.isLoading = true;
-      this.page++;
-      const resp = await this.fetchData();
-      this.data.total = resp.total;
-      this.data.rows = this.data.rows.concat(resp.rows);
-      this.isLoading = false;
-    },
-    handleScroll(dom) {
-      if (this.isLoading || !dom) {
-        return;
-      }
-      const range = 100;
-      const dec = Math.abs(dom.scrollTop + dom.clientHeight - dom.scrollHeight);
-      if (dec <= range) {
-        this.fetchMore();
-      }
-    },
-  },
-  created() {
-    this.$bus.$on("mainScroll", this.handleScroll);
-  },
-  destoryed() {
-    this.$bus.$off("mainScroll", this.handleScroll);
-  },
-  computed: {
-    hasMore() {
-      return this.data.rows.length < this.data.total;
     },
   },
 };
